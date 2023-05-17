cb16d9e44787d2558c2cca659b644371e68d3d6a
harpreet120 <harpreet.singh@novatec-gmbh.de>
2023-05-17 11:22:43 +0200

    Grundaufbau

---
+++ TestFrameWorkWI2020C/src/main/java/com/example/testframeworkwi2020c/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.testframeworkwi2020c;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
