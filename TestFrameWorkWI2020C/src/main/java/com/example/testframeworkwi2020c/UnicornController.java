fe856358ad6f89d74251a941b8c8e878bc7f94c1
Meike <wi20106@lehre.dhbw-stuttgart.de>
2023-05-18 12:29:32 +0200

    Erster Entwurf des Frontends

---
+++ TestFrameWorkWI2020C/src/main/java/com/example/testframeworkwi2020c/UnicornController.java
@@ -0,0 +1,15 @@
+package com.example.testframeworkwi2020c;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class UnicornController {
+    @FXML
+    private ChoiceBox<String> choiceBoxId;
+
+    @FXML
+    private TextArea outputAreaId;
+
+}
