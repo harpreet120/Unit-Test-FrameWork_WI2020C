5ce09e4e5d52fbc7ccfbadb530d867ba2e1de769
Meike <wi20106@lehre.dhbw-stuttgart.de>
2023-06-26 14:00:30 +0200

    Änderungen Mac

--- TestFrameWorkWI2020C/src/main/java/com/example/testframeworkwi2020c/HelloApplication.java
+++ TestFrameWorkWI2020C/src/main/java/com/example/testframeworkwi2020c/HelloApplication.java
@@ -17,7 +17,11 @@ public class HelloApplication extends Application {
         this.stage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("unicorn-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+    //    scene.getStylesheets().add("src/main/resources/com/example/testframeworkwi2020c/style.css");
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
         stage.setTitle("Testfamework");
+
         stage.setScene(scene);
         stage.show();
 
